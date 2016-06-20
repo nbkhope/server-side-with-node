a2791d6ce6fc28441c18d7d8ceafaee6bc708b19
nbkhope <ryumemaru@yahoo.com>
2016-06-19 20:49:27 -0700

    Add leaderRouter and promoRouter modules

---
+++ assignment-1/server.js
@@ -0,0 +1,30 @@
+// Require node modules
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+// Server settings
+var hostname = 'localhost';
+var port = 3000;
+
+// Call on Express
+var app = express();
+
+// Morgan logs the HTTP requests to the console
+app.use(morgan('dev'));
+
+// Express Routers
+var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter');
+var leaderRouter = require('./leaderRouter');
+
+// Apply the dishRouter to anything to do with /dishes
+app.use('/dishes', dishRouter);
+
+// Determine where to serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Initiate server
+app.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
