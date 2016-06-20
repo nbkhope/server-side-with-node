3e2af06355dd15326ce216092c0341336a1f3f6e
nbkhope <ryumemaru@yahoo.com>
2016-06-19 17:21:53 -0700

    Make server into an Express server serving static files under public/

--- node-express/server.js
+++ node-express/server.js
@@ -1,6 +1,7 @@
 // Require node modules
-var express = require('express'),
-    http = require('http');
+var express = require('express');
+// Morgan middleware
+var morgan = require('morgan');
 
 // Server settings
 var hostname = 'localhost';
@@ -9,18 +10,12 @@ var port = 3000;
 // Call on Express
 var app = express();
 
-app.use(function(req, res, next) {
-  // Print out the request headers
-  console.log(req.headers);
+app.use(morgan('dev'));
 
-  // Send a response
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body><h1>Hello World</h1></body></html>');
-});
+// Determine where to serve static files
+app.use(express.static(__dirname + '/public'));
 
-// Create the server using Express app
-var server = http.createServer(app);
-
-server.listen(port, hostname, function() {
+// Initiate server
+app.listen(port, hostname, function() {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
