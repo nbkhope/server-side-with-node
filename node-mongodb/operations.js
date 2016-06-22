0be91add23b5c665ee2c185314dca3834bff3655
nbkhope <ryumemaru@yahoo.com>
2016-06-22 10:55:59 -0700

    Add operations.js file with skeleton for module functions to insert/find/remove/delete document from MongoDB

---
+++ node-mongodb/operations.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+exports.insertDocument = function(db, document, collection, callback) {
+
+};
+
+exports.findDocuments = function(db, collection, callback) {
+
+};
+
+exports.removeDocument = function(db, document, collection, callback) {
+
+};
+
+exports.updateDocument = function(db, document, update, collection, callback) {
+
+};
